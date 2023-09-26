dc6938917fbc4f2a22903844774233e93ee17f30
nezhinskii <maksimnezhin@gmail.com>
2023-09-26 07:53:57 +0300

    flood fill revival

--- lib/bloc/main_bloc.dart
+++ lib/bloc/main_bloc.dart
@@ -27,6 +27,105 @@ class MainBloc extends Bloc<MainEvent, MainState> {
     ..strokeWidth = 1
     ..color = Colors.black;
 
+  void _floodFill(ui.Image image, ByteData byteData, GestureEvent gestureEvent){
+
+    final int width = image!.width;
+    final int height = image!.height;
+    Uint8List uint8List = byteData!.buffer.asUint8List();
+
+    final int targetPixelX = gestureEvent.position.dx.toInt();
+    final int targetPixelY = gestureEvent.position.dy.toInt();
+
+    final int targetPixelOffset = (targetPixelY * width + targetPixelX) * 4;
+
+    final Color targetColor = Color.fromRGBO(
+      uint8List[targetPixelOffset],
+      uint8List[targetPixelOffset + 1],
+      uint8List[targetPixelOffset + 2],
+      uint8List[targetPixelOffset + 3] / 255.0,
+    );
+
+    final List<Offset> points = [];
+    final List<List<bool>> visited = List.generate(
+      width,
+          (i) => List<bool>.filled(height, false),
+    );
+
+    final List<List<int>> stack = [];
+
+    stack.add([targetPixelX, targetPixelY]);
+
+    while (stack.isNotEmpty) {
+      final currentPoint = stack.removeLast();
+      final x = currentPoint[0];
+      final y = currentPoint[1];
+
+      if (x >= 0 && x < width && y >= 0 && y < height && !visited[x][y]) {
+        visited[x][y] = true;
+
+        final int currentPixelOffset = (y * width + x) * 4;
+        final Color currentPixelColor = Color.fromRGBO(
+          uint8List[currentPixelOffset],
+          uint8List[currentPixelOffset + 1],
+          uint8List[currentPixelOffset + 2],
+          uint8List[currentPixelOffset + 3] / 255.0,
+        );
+
+        if (currentPixelColor == targetColor) {
+          int left = x;
+          int right = x;
+
+          while (left >= 0 &&
+              Color.fromRGBO(
+                  uint8List[(y * width + left) * 4],
+                  uint8List[(y * width + left) * 4 + 1],
+                  uint8List[(y * width + left) * 4 + 2],
+                  uint8List[(y * width + left) * 4 + 3] / 255.0) ==
+                  targetColor) {
+            left--;
+          }
+
+          while (right < width &&
+              Color.fromRGBO(
+                  uint8List[(y * width + right) * 4],
+                  uint8List[(y * width + right) * 4 + 1],
+                  uint8List[(y * width + right) * 4 + 2],
+                  uint8List[(y * width + right) * 4 + 3] / 255.0) ==
+                  targetColor) {
+            right++;
+          }
+
+          for (int i = left + 1; i < right; i++) {
+            visited[i][y] = true;
+            var color = gestureEvent.style.color;
+            var offset = (y * width + i) * 4;
+            uint8List[offset] = color.red;
+            uint8List[offset + 1] = color.green;
+            uint8List[offset + 2] = color.blue;
+            uint8List[offset + 3] = color.alpha;
+            // points.add(Offset(i.toDouble(), y.toDouble()));
+
+            if (y > 0) {
+              stack.add([i, y - 1]);
+            }
+            if (y < height - 1) {
+              stack.add([i, y + 1]);
+            }
+          }
+        }
+      }
+    }
+    ui.Image? img;
+    ui.decodeImageFromPixels(uint8List,
+        width.toInt(),
+        height.toInt(),
+        ui.PixelFormat.rgba8888,
+        (image){
+          emit(FloodFillState([], image, null));
+        }
+    );
+  }
+
   void _onGestureUpdate(MainGestureUpdate event, Emitter emit) async {
     final List<GestureEvent> eventList = [];
     final gestureEvent =
@@ -43,10 +142,12 @@ class MainBloc extends Bloc<MainEvent, MainState> {
           eventList.addAll([state.gestureEvents.first, gestureEvent]);
         }
       case GestureEventType.panDown:
+        eventList.add(gestureEvent);
         if (state is FloodFillState) {
           bd = await state.canvasHistory?.toByteData();
+          _floodFill(state.canvasHistory!, bd!, gestureEvent);
+          return;
         }
-        eventList.add(gestureEvent);
     }
     if (state is FloodFillState) {
       var st = state as FloodFillState;
